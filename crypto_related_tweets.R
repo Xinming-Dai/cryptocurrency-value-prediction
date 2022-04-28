cc33e3d5e3fd8ac0dc599d20d94eacfcea3e0115
Xinming <daixinming0101@icloud.com>
2022-04-27 22:49:03 -0400

    build Tweet feature

--- crypto_related_tweets.R
+++ crypto_related_tweets.R
@@ -2,7 +2,9 @@
 setwd('/Users/daixinming/PycharmProjects/crypto_prediction/tweets')
 library(tidyverse)
 library(tidytext)
-source('tweets_analysis/bag_of_words.R')
+library(lubridate)
+
+source('cryptocurrency-value-prediction/bag_of_words.R')
 
 tweet_elon <- read_csv('data_tweets/tweet_elon.csv')
 
@@ -18,4 +20,13 @@ tweet_elon_crypto <-
   tweet_elon_token %>% 
   filter(words %in% bag_of_words)
 
-write.csv(tweet_elon_crypto, 'data_tweets/tweet_elon_crypto.csv')
+# write.csv(tweet_elon_crypto, 'data_tweets/tweet_elon_crypto.csv')
+
+# count how many tweets related to cryptocurrencies that Elon tweeted
+tweet <- 
+  tweet_elon_crypto %>% 
+  mutate(created_at = format(parse_date_time(created_at, orders = "ymd HMS"), format = "%Y-%m-%d")) %>% 
+  group_by(created_at) %>% 
+  mutate(num_tweets = n()) %>% 
+  distinct(created_at, num_tweets)
+
