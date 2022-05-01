15ac5a0efc677f552c235a43a0dd9ea1266e6637
Xinming Dai <daixinming0101@icloud.com>
2022-05-01 17:46:51 -0400

    updated logistic regression

--- analysis/model and analysis.R
+++ analysis/model and analysis.R
@@ -6,75 +6,102 @@ options("getSymbols.yahoo.warning"=FALSE)
 
 # Part I: Using tweets relates to cryptocurrency to build model and make prediction(easy model)-------------------------------------------------------------
 
-# crypto data from 2021-04-28 to 2022-04-10
+# crypto data from 2021-04-28 to 2022-04-26
 start_date <- "2021-04-28"
-until_date <- "2022-04-10"
+end_date <- "2022-04-26"
 
 # we want those three companies' cryptocurrency data
-tickers = c("ETH-USD", "BTC-USD", "DOGE-USD")
+tickers = c("ETH-USD", "BTC-USD", "DOGE-USD", "USDT-USD", "SOL-USD", "BCH-USD")
 
-
-# For each company, use tq_get() to obtain their stock price from 2021-04-28 to 2022-04-10
+# we set the outcome be 1 if the daily the absolute return is greater than or equal to 5%, 0 otherwise
+# For each company, use tq_get() to obtain their stock price from 2021-04-28 to 2022-04-26
 # then compute the difference between their open value and close value 
-doge <- 
-  tq_get("DOGE-USD",
-         from = start_date,
-         to = until_date,
-         get = "stock.prices") %>% 
-  mutate(doge = (close-open)/open) %>% 
- dplyr:: select(date, doge)
-
 eth <- 
   tq_get("ETH-USD",
          from = start_date,
-         to = until_date,
+         to = end_date,
          get = "stock.prices") %>% 
-  mutate(eth = (close-open)/open) %>% 
-  dplyr::select(date, eth)
+  mutate(eth_returns = pc_col(adjusted)) %>% 
+  mutate(eth = ifelse(abs(eth_returns) < 5, 0, 1)) %>% 
+  dplyr::select(date, eth_returns, eth)
+
+doge <- 
+  tq_get("DOGE-USD",
+         from = start_date,
+         to = end_date,
+         get = "stock.prices") %>% 
+  mutate(doge = pc_col(adjusted)) %>% 
+  mutate(doge = ifelse(abs(doge) < 5, 0, 1)) %>% 
+  dplyr:: select(date, doge)
 
 btc <- 
   tq_get("BTC-USD",
          from = start_date,
-         to = until_date,
+         to = end_date,
          get = "stock.prices") %>% 
-  mutate(btc = (close-open)/open) %>% 
+  mutate(btc = pc_col(adjusted)) %>% 
+  mutate(btc = ifelse(abs(btc) < 5, 0, 1)) %>% 
   dplyr::select(date, btc)
 
+sol <- 
+  tq_get("SOL-USD",
+         from = start_date,
+         to = end_date,
+         get = "stock.prices") %>% 
+  mutate(sol = pc_col(adjusted)) %>% 
+  mutate(sol = ifelse(abs(sol) < 5, 0, 1)) %>% 
+  dplyr::select(date, sol)
+
+bch <- 
+  tq_get("BCH-USD",
+         from = start_date,
+         to = end_date,
+         get = "stock.prices") %>% 
+  mutate(bch = pc_col(adjusted)) %>% 
+  mutate(bch = ifelse(abs(bch) < 5, 0, 1)) %>% 
+  dplyr::select(date, bch)
+
 # finally, we join them together with the number of tweets that elon musk post (those only relates to cryptocurrency)
-doge <- 
-  doge %>% 
-  left_join(eth) %>% 
+eth <- 
+  eth %>% 
+  left_join(doge) %>% 
   left_join(btc) %>% 
   left_join(tweet) %>% 
+  left_join(sol) %>% 
+  left_join(bch) %>% 
   mutate(num_tweets = replace_na(num_tweets, 0))
+eth <- eth[-1, ]
 
 ## Simple logistic regression---------------------------------------------------------------
 # we first fit the model with a simple logistic regression and calculate its AUC score
-# we set the outcome be 1 if the daily return(close-open) is greater than 0.1, 0 otherwise
-doge_log <- 
-  doge %>% 
-  mutate(doge = ifelse(abs(doge) > 0.1, 1, 0))
 
 # fit a logistic regression 
-logistic <- glm(doge ~ eth + btc + num_tweets, data = doge_log, family = 'binomial')
+logistic <- glm(eth ~ doge + btc + sol + bch + num_tweets, data = eth, family = 'binomial')
 summary(logistic)
 
-# fit a logistic regression with interaction terms 
-logistic_inter <- glm(doge ~ eth + btc + num_tweets + eth*num_tweets + btc*num_tweets, data = doge_log, 
-                  family = 'binomial')
-summary(logistic_inter)
-
 # make prediction for simple logistic regression and logi with interaction terms
-logistic_prob <- predict(logistic, doge_log)
-logistic_inter_prob <- predict(logistic_inter, doge_log)
+logistic_prob <- predict(logistic, eth_log)
 
 # AUC
-logistic_pred <- prediction(logistic_prob, doge_log$doge)
-logistic_inter_pred <- prediction(logistic_inter_prob, doge_log$doge)
+logistic_pred <- prediction(logistic_prob, eth_log$eth)
 logisitc_performace <- performance(logistic_pred, 'auc')
-logistic_inter_performace <- performance(logistic_inter_pred, 'auc')
 cat('The AUC score of the simple logisitc is ', logisitc_performace@y.values[[1]], "\n")
-cat('The AUC score of the logisitc with interaction term is ', logistic_inter_performace@y.values[[1]], "\n")
+
+# plot tweets and returns
+eth_plot <- 
+  eth %>% 
+  mutate(eth = ifelse(abs(eth_returns) < 5, 0, abs(eth_returns))/3)
+
+p1 <- 
+  ggplot(data = eth_plot, aes(x = date)) +
+  geom_line(aes(y = eth, colour = "eth_abs_returns/3")) +
+  geom_line(aes(y = num_tweets, colour = "num_tweets")) +
+  scale_colour_manual("", 
+                      breaks = c("eth_abs_returns/3", "num_tweets"),
+                      values = c("blue", "red"))
+p1
+
+ggsave(plot = p1, file = '../figures/tweets_and_returns.png', height = 5, width = 10)
 
 # Part II: Introducing time series in order to do a better fit (complex models)-----------------------------------------------------
 
