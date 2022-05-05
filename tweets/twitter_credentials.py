c983fa9e19b6addc9311f9e459818ab563280abe
Xinming Dai <daixinming0101@icloud.com>
2022-05-05 01:04:24 -0400

    deleted credentials

--- tweets/twitter_credentials.py
+++
@@ -1,7 +0,0 @@
-# Xinming Dai Twitter API key and tokens
-
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMgcaAEAAAAAW%2B4DocTzBf5oI6cokz0r2RPZ00w%3DYVAf9vdkf6akZrr78QQTP7TAOyEKpeFYKWbdKqyuzZ8UNmz94t'
-consumer_key = 'y4wL773L7pNK1cegSkQJd6p3l'
-consumer_secret = 'ySuQI3jy5gWjLlH5hqjmuvLQMNnyj74bYP44ii3rusukYcsoLM'
-access_token = '1126990870002130944-lPXoOszGCGop3kK3hJdwhDWWOcpIzu'
-access_token_secret = 'dB0ZH8r2nACeWswSEBcDJBbELSrWKOx1xr42DHAySquHX'
